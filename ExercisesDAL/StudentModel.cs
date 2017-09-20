1a9c2d260762f8f233146c533fec51cc27a3462f
Jeremy P-K <jeremy.i.pk@gmail.com>
2017-09-20 11:12:12 -0400

    Finished Lab5a

--- ExercisesDAL/StudentModel.cs
+++ ExercisesDAL/StudentModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
+using System.Collections.Generic;
 
 namespace ExercisesDAL
 {
@@ -22,6 +23,104 @@ namespace ExercisesDAL
             }
 
             return selectedStudent;
+        }
+
+        public Student GetById(int id)
+        {
+            Student selectedStudent = null;
+
+            try
+            {
+                SomeSchoolContext ctx = new SomeSchoolContext();
+                selectedStudent = ctx.Students.FirstOrDefault(stu => stu.Id == id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " +
+                    MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+
+            return selectedStudent;
+        }
+
+        public List<Student> GetAll()
+        {
+            List<Student> allStudents = new List<Student>();
+
+            try
+            {
+                SomeSchoolContext ctx = new SomeSchoolContext();
+                allStudents = ctx.Students.ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " +
+                    MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+
+            return allStudents;
+        }
+
+        public int Add(Student newStudent)
+        {
+            try
+            {
+                SomeSchoolContext ctx = new SomeSchoolContext();
+                ctx.Students.Add(newStudent);
+                ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " +
+                    MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+
+            return newStudent.Id;
+        }
+
+        public int Update(Student updatedStudent)
+        {
+            int studentsUpdated = -1;
+
+            try
+            {
+                SomeSchoolContext ctx = new SomeSchoolContext();
+                Student currentStudent = ctx.Students.FirstOrDefault(stu => stu.Id == updatedStudent.Id);
+                ctx.Entry(currentStudent).CurrentValues.SetValues(updatedStudent);
+                studentsUpdated = ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " +
+                    MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+
+            return studentsUpdated;
+        }
+
+        public int Delete(int id)
+        {
+            int studentsDeleted = -1;
+
+            try
+            {
+                SomeSchoolContext ctx = new SomeSchoolContext();
+                Student currentStudent = ctx.Students.FirstOrDefault(stu => stu.Id == id);
+                ctx.Students.Remove(currentStudent);
+                studentsDeleted = ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " +
+                    MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+
+            return studentsDeleted;
         }
     }
 }
--- ExercisesTests/StudentModelTests.cs
+++ ExercisesTests/StudentModelTests.cs
@@ -2,26 +2,78 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using ExercisesDAL;
+using System.Collections.Generic;
 
 namespace ExercisesTests
 {
     [TestClass]
     public class StudentModelTests
     {
+        //[TestMethod]
+        //public void StudentModelGetbyLastnameShouldReturnStudent()
+        //{
+        //    StudentModel model = new StudentModel();
+        //    Student someStudent = model.GetByLastname("Peterson-Katz");
+        //    Assert.IsNotNull(someStudent);
+        //}
+
+        //[TestMethod]
+        //public void StudentModelGetbyLastnameShouldNotReturnStudent()
+        //{
+        //    StudentModel model = new StudentModel();
+        //    Student someStudent = model.GetByLastname("Nosey");
+        //    Assert.IsNull(someStudent); 
+        //}
+
         [TestMethod]
-        public void StudentModelGetbyLastnameShouldReturnStudent()
+        public void StudentModelGetAllShouldReturnList()
         {
             StudentModel model = new StudentModel();
-            Student someStudent = model.GetByLastname("Peterson-Katz");
-            Assert.IsNotNull(someStudent);
+            List<Student> allStudents = model.GetAll();
+            Assert.IsTrue(allStudents.Count > 0);
         }
 
         [TestMethod]
-        public void StudentModelGetbyLastnameShouldNotReturnStudent()
+        public void StudentModelAddShouldReturnNewId()
         {
             StudentModel model = new StudentModel();
-            Student someStudent = model.GetByLastname("Nosey");
-            Assert.IsNull(someStudent); 
+            Student newStudent = new Student();
+            newStudent.Title = "Mr.";
+            newStudent.FirstName = "Test";
+            newStudent.LastName = "Student";
+            newStudent.Email = "ts@abc.com";
+            newStudent.PhoneNo = "(555)555-5551";
+            newStudent.DivisionId = 10;
+            int newId = model.Add(newStudent);
+            Assert.IsTrue(newId > 0);
+        }
+
+        [TestMethod]
+        public void StudentModelGetbyIdShouldReturnStudent()
+        {
+            StudentModel model = new StudentModel();
+            Student someStudent = model.GetByLastname("Student");
+            Student anotherStudent = model.GetById(someStudent.Id);
+            Assert.IsNotNull(anotherStudent);
+        }
+
+        [TestMethod]
+        public void StudentModelUpdateShouldReturnOne()
+        {
+            StudentModel model = new StudentModel();
+            Student updateStudent = model.GetByLastname("Student");
+            updateStudent.Email = "ts@xyz.com";
+            int StudentsUpdated = model.Update(updateStudent);
+            Assert.IsTrue(StudentsUpdated == 1);
+        }
+
+        [TestMethod]
+        public void StudentModelDeleteShouldReturnOne()
+        {
+            StudentModel model = new StudentModel();
+            Student deleteStudent = model.GetByLastname("Student");
+            int StudentsDeleted = model.Delete(deleteStudent.Id);
+            Assert.IsTrue(StudentsDeleted == 1);
         }
     }
 }
